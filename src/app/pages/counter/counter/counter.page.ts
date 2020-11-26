2b9634783ad115532720a5a4289ab851fd2e3da9
tearz007 <54117577+tearz007@users.noreply.github.com>
2020-11-26 04:02:25 -0800

    26/11/2020

--- src/app/pages/counter/counter/counter.page.ts
+++ src/app/pages/counter/counter/counter.page.ts
@@ -10,24 +10,54 @@ export class CounterPage implements OnInit {
 
   homeTeam
   awayTeam
-  goal
+  goal1
+  goal2
   constructor(private service: CounterService) {
     this.homeTeam = this.service.homeTeam;
     this.awayTeam = this.service.awayTeam;
+    this.goal1 = this.service.team1Goal;
+    this.goal2 = this.service.team2Goal;
   }
 
   ngOnInit() {
   }
-
-  setGoal(team) {
-
+  setNum(team) {
     if (team == "home") {
-      this.service.team1Goal = this.service.team1Goal + 1
+
+      this.service.setNumber(team);
+      console.log("Goal for "+this.homeTeam)
+      this.goal1 = this.service.team1Goal;
     } else {
-      this.service.team2Goal = this.service.team2Goal + 1
+      this.service.setNumber(team);
+      console.log("Goal for "+this.awayTeam)
+      this.goal2 = this.service.team2Goal;
     }
-   let val=this.service.team1.goal
-    console.log(val)
+  }
+  setOffside(team) {
+    if (team == "home") {
+      console.log(this.homeTeam +" is offside")
+     
+    } else {
+      console.log(this.awayTeam +" is offside")
+     
+    }
   }
 
+  setYellow(team) {
+    if (team == "home") {
+      console.log(this.homeTeam +" got a yellow card") 
+    } else {
+      console.log(this.awayTeam +" got a yellow card")  
+    }
+  }
+
+  setRed(team) {
+    if (team == "home") {
+      console.log(this.homeTeam +" got a red card")
+    } else {
+      console.log(this.awayTeam +" got a red card")
+    }
+  }
+
+  
 }
--- src/app/services/counter/counter.service.ts
+++ src/app/services/counter/counter.service.ts
@@ -6,32 +6,33 @@ import { Injectable } from '@angular/core';
 export class CounterService {
   homeTeam;
   awayTeam;
+  team1Goal=0;
+  team2Goal=0;
 
-  team1Yellw=0
-  team1Red=0
   team1Offside=0
-  team1Goal=0
-  team2Yellw=0
-  team2Red=0
   team2Offside=0
-  team2Goal=0
-
-  team1={yellow:this.team1Yellw,red:this.team1Red,offside:this.team1Offside,goal:this.team1Goal}
-  team2={yellow:this.team2Yellw,red:this.team2Red,offside:this.team2Offside,goal:this.team2Goal}
   constructor() { }
 
   setTeams(home, away) {
     this.homeTeam = home;
     this.awayTeam = away;
   }
+  
+  setNumber(team){ 
+    if (team=="home") {
+      this. team1Goal = this. team1Goal +1;
+    }else{
+      this. team2Goal = this. team2Goal +1;
+    }
+  }
 
- getTeam1(){
-   return this.team1;
- }
- getTeam2(){
-  return this.team2;
- }
-
+  setOffside(team){ 
+    if (team=="home") {
+      this.team1Offside = this.team1Offside +1;
+    }else{
+      this.team2Offside = this.team2Offside +1;
+    }
+  }
 
 
 }
